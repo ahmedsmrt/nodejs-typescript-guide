0efc66a56d065dd2c1a82ee1773c541d8afc7b35
agasimdev <agasim@archeredu.com>
2024-06-08 13:33:48 -0400

    dev: worked with interface/type creation, function type and defining return types for function

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,40 +1,62 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './assets/css/output.css'
 
+
+let name: string;
+//  the | is the union operator
+let age: number | string;
+let isStudent: boolean = true;
+let hobbies: string[];
+let role: [number, string];
+
+
+// Ways to define a func
+// When defining the return type never returns nothing while void returns null;
+let printName: (name: string) => never;
+
+// Reccomended instead of using the any type
+let personName: unknown;
+
+// Two types of aliases to define objects type & interface
+// Interface makes it easier to combine type guards from different interfaces
+interface User extends Person {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+}
+
+
+type Users = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+}
+
+interface Person {
+  name: string;
+  age?: number;
+}
+
+
+// type Person = {
+//   name: string;
+//   age?: number;
+// }
+
+// let person: Person = {
+//   name: "ahmed",
+//   age: 32
+// }
+
+// let lotsOfPeople: Person[];
+
 function App() {
-  const [message, setMessage] = useState('What is up fam')
-  const [msg, setMsg] = useState('What')
-
-
-
-  const sendMsg = async () => {
-    try {
-      // Await response from fetch call
-      const apiUrl = import.meta.env.VITE_REACT_APP_URL || '';
-      const response: Response = await fetch(`${apiUrl}/api/hello`);
-      const res: Response = await fetch(`${apiUrl}/api/what`);
-
-      // Await the text conversion from the response
-      const text: string = await response.text();
-      const textTwo: string = await res.text();
-
-      // Set the message state with the fetched text
-      setMessage(text);
-      setMsg(textTwo);
-    } catch (error) {
-      console.error('Error fetching message:', error);
-    }
-  }
-
   return (
     <>
       <main className="flex flex-col justify-center w-full h-full items-center mx-auto">
-        Your Server Message fam is: {message}<br></br>
-        Your Second Server Message fam is: {msg}
-
-        <button className="btn bg-primary border-0" onClick={sendMsg}>Click Me Shawrty!</button>
+        Hello {name}!!
       </main>
     </>
   )
--- client/src/main.js
+++ client/src/main.js
@@ -2,5 +2,5 @@ import { jsx as _jsx } from "react/jsx-runtime";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import './assets/css/index.css';
+import './assets/css/output.css';
 ReactDOM.createRoot(document.getElementById('root')).render(_jsx(React.StrictMode, { children: _jsx(App, {}) }));
